5718970a5e033a554cbb0db8384dac6cc0319b76
natacharyannyoliveira-star <natacha.ryanny.oliveira@escola.pr.gov.br>
2026-06-19 09:02:01 -0300

    Add filterMotos function to filter card display

---
+++ script.js
@@ -0,0 +1,15 @@
+function filterMotos(category) {
+  const cards = document.querySelectorAll('.card');
+
+  cards.forEach(card => {
+    if (category === 'all') {
+      card.style.display = 'block';
+    } else {
+      if (card.classList.contains(category)) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    }
+  });
+}
